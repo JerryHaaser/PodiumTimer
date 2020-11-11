dbc13cff7c8795ec677ead0e528a3b9c3a91505f
Jerry Haaser <jerry.haaser@hotmail.com>
2020-11-10 20:30:02 -0600

    Set up test UI for cclock

---
+++ PodiumTimer/PodiumTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PodiumTimer
+//
+//  Created by Jerry haaser on 11/10/20.
+//  Copyright © 2020 Jerry haaser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
